4c01891b72e65a42fd10038e3bab7b68e83d6893
Evan Su <evan@hexacyanide.com>
2018-01-19 18:56:21 -0500

    Apply project naming conventions.

--- pjusurvey/pjusurvey/wsgi.py
+++ pjusurvey/pjusurvey/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PJUSurvey project.
+WSGI config for pjusurvey project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PJUSurvey.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pjusurvey.settings")
 
 application = get_wsgi_application()
